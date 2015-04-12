99fca4d41fe8e2220e204ed7d8ebe8267801feac
medtraf <ertan.ornek@diodostransportation.com>
2015-04-12 18:39:44 -0500

    Delete explore.R

--- explore.R
+++
@@ -1,5 +0,0 @@
-setwd("C:/Users/ertan.ornek/Desktop/coursera/exploratory")
-
-myData<-read.csv(file = "household_power_consumption.txt" ,header = TRUE,sep=";")
-
-mySub<-subset(myData,as.Date(myData$Date,format = "%d/%m/%Y")==as.Date("2007/2/1") | as.Date(myData$Date,format = "%d/%m/%Y")==as.Date("2007/2/2") )
